c29d4f5dd33c6e7276ed67c2f84a782df5544ac4
Sajal Regmi <76418843+sajalregmi@users.noreply.github.com>
2025-03-05 01:09:06 -0700

    Update PencilKit.tsx

--- src/component/PencilKit.tsx
+++ src/component/PencilKit.tsx
@@ -1,6 +1,7 @@
 import { type ForwardedRef, forwardRef } from 'react';
 import { type ColorValue, Text, type ViewProps } from 'react-native';
 import type { DirectEventHandler, WithDefault } from 'react-native/Libraries/Types/CodegenTypes';
+import { CanvasViewDrawingDidChangeEvent } from '../spec/RNPencilKitNativeComponent';
 
 export type PencilKitProps = {
   alwaysBounceVertical?: boolean;
@@ -9,6 +10,7 @@ export type PencilKitProps = {
   backgroundColor?: ColorValue;
   drawingPolicy?: WithDefault<'default' | 'anyinput' | 'pencilonly', 'default'>;
   isOpaque?: boolean;
+  imageURL?: string;
 
   onToolPickerVisibilityDidChange?: DirectEventHandler<{}>;
   onToolPickerIsRulerActiveDidChange?: DirectEventHandler<{}>;
@@ -16,7 +18,7 @@ export type PencilKitProps = {
   onToolPickerSelectedToolDidChange?: DirectEventHandler<{}>;
   onCanvasViewDidBeginUsingTool?: DirectEventHandler<{}>;
   onCanvasViewDidEndUsingTool?: DirectEventHandler<{}>;
-  onCanvasViewDrawingDidChange?: DirectEventHandler<{}>;
+  onCanvasViewDrawingDidChange?: DirectEventHandler<CanvasViewDrawingDidChangeEvent>;
   onCanvasViewDidFinishRendering?: DirectEventHandler<{}>;
 } & ViewProps;
 export type PencilKitTool =
@@ -36,6 +38,7 @@ export type PencilKitRef = {
   hideToolPicker: () => void;
   redo: () => void;
   undo: () => void;
+  loadStroke: (strokeBase64: string) => void;
   setTool: (params: { toolType: PencilKitTool; width?: number; color?: ColorValue }) => void;
   saveDrawing: (path: string) => Promise<string>;
   loadDrawing: (path: string) => Promise<void>;
@@ -43,6 +46,7 @@ export type PencilKitRef = {
   getBase64PngData: (params?: { scale?: number }) => Promise<string>;
   getBase64JpegData: (params?: { scale?: number; compression?: number }) => Promise<string>;
   loadBase64Data: (base64: string) => Promise<void>;
+
 };
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const PencilKit = forwardRef((props: PencilKitProps, ref: ForwardedRef<PencilKitRef>) => {
